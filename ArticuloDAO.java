9286294bda0265d757bc7a15d71824cac919e15a
frpascua <francisco-jose.pascual@unirioja.es>
2024-03-19 12:46:30 +0100

    Initial commit

---
+++ ArticuloDAO.java
@@ -0,0 +1,66 @@
+package es.unirioja.paw.db;
+
+import es.unirioja.paw.controller.ArticuloSearchCriteria;
+import es.unirioja.paw.model.Articulo;
+import es.unirioja.paw.model.ExcepcionDeAplicacion;
+import es.unirioja.paw.model.TipoArticulo;
+import es.unirioja.paw.pagination.PageNumberPagination;
+import java.util.List;
+
+public interface ArticuloDAO {
+
+    /**
+     * @return Todos los articulos
+     * @throws ExcepcionDeAplicacion 
+     */
+    public List<Articulo> findAll() throws ExcepcionDeAplicacion;
+
+    /**
+     * @param codigo Código de artículo
+     * @return Articulo
+     * @throws ExcepcionDeAplicacion 
+     */
+    public Articulo findOneByCodigo(String codigo) throws ExcepcionDeAplicacion;
+
+    /**
+     * @param pageNumber Número de página a mostrar
+     * @param pageSize Número de artículos a mostrar en cada página
+     * @return Lista de articulos a mostrar en la pagina
+     * @throws ExcepcionDeAplicacion
+     */
+    public List<Articulo> findByPage(int pageNumber, int pageSize) throws ExcepcionDeAplicacion;
+
+    /**
+     * @return Todos los tipos de artículos
+     * @throws ExcepcionDeAplicacion 
+     */
+    public List<TipoArticulo> findTiposArticulo() throws ExcepcionDeAplicacion;
+
+    /**
+     *
+     * @param pageSize Número de artículos a mostrar en cada página
+     * @param currentPage Número de página actual
+     * @return Información para el paginador
+     * @throws ExcepcionDeAplicacion
+     */
+    public PageNumberPagination buildPageNumberPagination(int pageSize, int currentPage) throws ExcepcionDeAplicacion;
+
+    /**
+     * 
+     * @param criteria Criterios de búsqueda
+     * @param pageSize Número de artículos a mostrar en cada página
+     * @param currentPage Número de página actual
+     * @return
+     * @throws ExcepcionDeAplicacion 
+     */
+    public List<Articulo> findByCriteria(ArticuloSearchCriteria criteria, int pageSize, int currentPage) throws ExcepcionDeAplicacion;
+
+    /**
+     * @param criteria Criterios de búsqueda
+     * @param pageSize Número de artículos a mostrar en cada página
+     * @param currentPage Número de página actual
+     * @return Información para el paginador
+     * @throws ExcepcionDeAplicacion 
+     */
+    public PageNumberPagination buildPageNumberPagination(ArticuloSearchCriteria criteria, int pageSize, int currentPage) throws ExcepcionDeAplicacion;
+}
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# practica05-anexo
+# Práctica 5
+
+Anexo de código de ayuda para la realización de la práctica.
+
+
+## Sesión 1
+
+# Búsqueda de artículos
+- `ArticuloSearchCriteria`
+- `ArticuloDAO`
+
+
+## Sesión 2
+
+### Comprobación usuario/contraseña
+
+```java
+public class UsuarioDaoMySQL implements UsuarioDAO {
+
+	public Usuario findUsuarioByExample(Usuario example) throws ExcepcionDeAplicacion 
+	{
+		// TODO
+	}
+
+    private String encodeValue(String value, String algorithm) {
+        byte[] bytes = value.getBytes();
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance(algorithm);
+        } catch (Exception exception) {
+            logger.error("Error getting MessageDigest instance", exception);
+            return value;
+        }
+        messageDigest.reset();
+        messageDigest.update(bytes);
+        byte[] digestBytes = messageDigest.digest();
+        StringBuffer stringBuffer = new StringBuffer();
+        for (byte b = 0; b < digestBytes.length; b++) {
+            if ((digestBytes[b] & 0xFF) < 16) {
+                stringBuffer.append("0");
+            }
+            stringBuffer.append(Long.toString((digestBytes[b] & 0xFF), 16));
+        }
+        return stringBuffer.toString();
+    }
+
+}
+```
